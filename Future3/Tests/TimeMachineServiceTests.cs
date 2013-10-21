330dac30174b85f0b6c5b272b7736b1cbf66676f
blanciq <pawel.olesiejuk@gmail.com>
2013-10-21 07:05:30 +0200

    test

---
+++ Future3/Tests/TimeMachineServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Future3.Models;
+using Future3.Services;
+using NUnit.Framework;
+
+namespace Future3.Tests
+{
+    [TestFixture]
+    public class TimeMachineServiceTests
+    {
+        private TimeMachineService _service;
+
+        [SetUp]
+        public void Before()
+        {
+            _service = new TimeMachineService();
+        }
+
+        [Test]
+        public void ShouldSendOneEmail()
+        {
+            TimeMachineService.Requests.Add(new EmailSendModel
+                {
+                    Email = "pawel.olesiejuk@goyello.com",
+                    Content = "Hello Pawel!",
+                    Date = DateTime.Now.Subtract(TimeSpan.FromMinutes(1))
+                });
+
+            _service.SingleRun();
+        }
+    }
+}
